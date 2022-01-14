9e507f4a312bf7adc1b6256cca28fac7a8086e86
zmdk154112 <sh01475@naver.com>
2022-01-14 17:02:17 +0900

    generic view

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\ai-013\pycharmprojects\django-project\mysite\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
